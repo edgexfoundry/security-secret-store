faf0cd5e5e66c33911c915a15738bd8bb0105447
Tony Espy <espy@canonical.com>
2018-07-20 10:03:40 -0400

    [snap] Fix hard-coded /vault paths in scripts
    The path /vault was hard-coded in a number of shell scripts,
    which made it difficult to specify alternate paths required
    when running EdgeX in a snap. This change allows the snap
    to specify _VAULT_DIR on the command line when running the
    scripts.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- vault-kong.sh
+++ vault-kong.sh
@@ -35,7 +35,7 @@ function houseKeeping() {
 #===================================== MAIN ====================================
 
 # Variables and parameters
-_VAULT_DIR="/vault"
+_VAULT_DIR=${_VAULT_DIR:-/vault}
 _VAULT_CONFIG_DIR="${_VAULT_DIR}/config"
 _VAULT_PKI_DIR="${_VAULT_DIR}/pki"
 _VAULT_FILE_DIR="${_VAULT_DIR}/file"
@@ -63,13 +63,14 @@ _VAULT_SVC="edgex-vault"
 _EDGEX_DOMAIN=""
 _VAULT_PORT="8200"
 _VAULT_API_PATH_KONG="/v1/secret/edgex/pki/tls/${_KONG_SVC}"
+_PKI_SETUP_KONG_ENV=${_PKI_SETUP_KONG_ENV:-pki-setup-config-kong.env}
 
 houseKeeping # temp files and payloads
 
 # Generate Kong PKI/TLS materials if they haven't been already...
 if [[ (! -f ${_KONG_PEM}) || (! -f ${_KONG_SK}) ]]; then
     echo ">> (3) Create PKI materials for Kong TLS server certificate"
-    /vault/pki-setup.sh /vault/pki-setup-config-kong.env
+    ${_VAULT_DIR}/pki-setup.sh ${_PKI_SETUP_KONG_ENV}
     chown vault:vault ${_CA_DIR}/${_KONG_SVC}.*
 else
     echo ">> (3) PKI materials for Kong TLS server certificate already created"
